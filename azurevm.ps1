05e37e7e9761a3bd23b28c0f51c8ae389dffa290
viveksan07 <viveksan07@gmail.com>
2024-08-02 14:13:02 -0400

    first commit

---
+++ azurevm.ps1
@@ -0,0 +1,34 @@
+# Log in to Azure using device authentication
+Connect-AzAccount 
+
+# Create a resource group
+$resourceGroupName = "test-rg"
+$location = "EastUS2"
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create a virtual network and a subnet
+$vnetName = "vnet-1"
+$subnetName = "subnet-1"
+$vnet = New-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Location $location -Name $vnetName -AddressPrefix "10.0.0.0/16"
+Add-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix "10.0.1.0/24" -VirtualNetwork $vnet
+$vnet | Set-AzVirtualNetwork
+
+# Get the virtual network and subnet
+$vnet = Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroupName
+$subnet = $vnet.Subnets | Where-Object { $_.Name -eq $subnetName }
+
+# Create a network interface for the VM
+$nicName = "nic-2"
+$nic = New-AzNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName -Location $location -SubnetId $subnet.Id
+
+# Create a virtual machine configuration
+$vmName = "MyVM"
+$credential = Get-Credential
+
+$vmConfig = New-AzVMConfig -VMName $vmName -VMSize "Standard_B2s" | `
+    Set-AzVMOperatingSystem -Windows -ComputerName $vmName -Credential $credential | `
+    Set-AzVMSourceImage -PublisherName "MicrosoftWindowsDesktop" -Offer "windows-11" -Skus "win11-22h2-pro" -Version "latest" | `
+    Add-AzVMNetworkInterface -Id $nic.Id
+
+# Create the virtual machine
+New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig
