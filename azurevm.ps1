101bdf59c4a5e17fd47aa17fea55e7a94a7eebbd
viveksan07 <viveksan07@gmail.com>
2024-08-03 20:58:17 +0000

    commit from linux

--- azurevm.ps1
+++ azurevm.ps1
@@ -3,23 +3,10 @@ Connect-AzAccount -Identity
 
 # Create a resource group
 $resourceGroupName = "test1-rg"
-$location = "EastUS2"
+$location = "eastus2"
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-# Create a virtual network and a subnet
-$vnetName = "vnet-1"
-$subnetName = "subnet-1"
-$vnet = New-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Location $location -Name $vnetName -AddressPrefix "10.0.0.0/16"
-Add-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix "10.0.1.0/24" -VirtualNetwork $vnet
-$vnet | Set-AzVirtualNetwork
 
-# Get the virtual network and subnet
-$vnet = Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroupName
-$subnet = $vnet.Subnets | Where-Object { $_.Name -eq $subnetName }
-
-# Create a network interface for the VM
-$nicName = "nic-2"
-$nic = New-AzNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName -Location $location -SubnetId $subnet.Id
 
 # Retrieve the credential from Azure Automation
 $credential = Get-AutomationPSCredential -Name "VMAdminCredential"
@@ -28,12 +15,17 @@ if (-not $credential) {
 }
 
 # Create a virtual machine configuration
-$vmName = "MyVM"
+$vmParams = @{
+  ResourceGroupName = 'test1-rg'
+  Name = 'MyVM'
+  Location = 'eastus2'
+  ImageName = "MicrosoftWindowsDesktop:windows-11:win11-22h2-pro:latest"
+  PublicIpAddressName = 'tutorialPublicIp'
+  Credential = $credential
+  OpenPorts = 3389
+  Size = 'Standard_B2s'
+}
 
-$vmConfig = New-AzVMConfig -VMName $vmName -VMSize "Standard_B2s" | `
-    Set-AzVMOperatingSystem -Windows -ComputerName $vmName -Credential $credential | `
-    Set-AzVMSourceImage -PublisherName "MicrosoftWindowsDesktop" -Offer "windows-11" -Skus "win11-22h2-pro" -Version "latest" | `
-    Add-AzVMNetworkInterface -Id $nic.Id
 
 # Create the virtual machine
-New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig
+New-AzVM @vmParams    
